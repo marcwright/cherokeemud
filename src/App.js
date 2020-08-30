8798ae01768d6204acb7c8484066f1a49111a6a4
Marc Wright <marc@generalassemb.ly>
2020-08-30 11:05:15 -0500

    push to heroku

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Alert } from "react-bootstrap";
 
 function App() {
+  const test = [
+    "primary",
+    "secondary",
+    "success",
+    "danger",
+    "warning",
+    "info",
+    "light",
+    "dark",
+  ].map((variant, idx) => (
+    <Alert key={idx} variant={variant}>
+      This is a {variant} alert—check it out!
+    </Alert>
+  ));
+
   return (
     <div className="App">
+      {test}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
